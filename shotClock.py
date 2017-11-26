673d8629076ae559cf19c4c6a8909c6e2febe8e1
unknown <sriram.ravula@utexas.edu>
2017-11-25 23:45:30 -0600

    changed shot_clock to stop editing, deleted redundant indeces in FINAL.csv

--- shotClock.py
+++ shotClock.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np
-data = pd.read_csv('./shot_logs.csv',sep=',')
+data = pd.read_csv('./FINAL.csv',sep=',')
 shot_clock = data['SHOT_CLOCK']
 game_clock = data['GAME_CLOCK']
 def get_sec(time_str):
@@ -11,16 +11,20 @@ def get_sec(time_str):
         return int(m)*60 + int(s)        
 no_shotclock_count = 0
 bad_count = 0
+print("data", data.shape)
 for idx,n in enumerate(shot_clock):
     if np.isnan(n):
         no_shotclock_count = no_shotclock_count + 1
         if (get_sec(game_clock[idx]) > 24):
             bad_count = bad_count + 1
+            data.drop(idx, inplace = True)
             #print (idx,game_clock[idx])
-        else:
+            
+        #else:
             #print (game_clock[idx], shot_clock[idx])
-            data['SHOT_CLOCK'][idx] = (get_sec(game_clock[idx]))
+            #data['SHOT_CLOCK'][idx] = (get_sec(game_clock[idx]))
             #print (shot_clock[idx],get_sec(game_clock[idx]))
-            #print (idx)
-data.to_csv('updated_shotlogs.csv')
-print (count)
+#data.to_csv('updated_shotlogs.csv')
+print("bad count", bad_count)
+print("total count", no_shotclock_count)
+print("data", data.shape)
