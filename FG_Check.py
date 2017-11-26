ea0b2101f6bddfdd727f3791bc0f909d20b336fc
SaiIsAwesome <saifromtexas@utexas.edu>
2017-11-26 12:26:59 -0600

    Jeff Taylor Prob fixed

--- FG_Check.py
+++ FG_Check.py
@@ -6,7 +6,15 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns; sns.set()
 from sklearn.model_selection import train_test_split
-data = pd.read_csv('./FINAL.csv',sep=',')
+data = pd.read_csv('./DUMMY_CODED.csv',sep=',')
+print(data['DBPM'].isnull().sum())
+data['DBPM'] = pd.to_numeric(data['DBPM'], errors='coerce')
+arr = pd.isnull(data).any(1).nonzero()[0]
+print(arr)
+#for idx in arr:
+#    data['DBPM'][idx] = -1.2
+#data.to_csv('DUMMY_CODED.csv', index=False)
+"""
 fgTotal = 0
 fgm = 0
 for idx, player in enumerate(data['player_id']):
@@ -17,3 +25,4 @@ for idx, player in enumerate(data['player_id']):
 print(fgTotal)
 print(fgm)
 print(fgm / float(fgTotal))
+"""
