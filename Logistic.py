05ed386aa4df66a8fcd8ba4b374558061c1fe4c7
unknown <sriram.ravula@utexas.edu>
2017-11-28 01:27:17 -0600

    added ROC to logistic

--- Logistic.py
+++ Logistic.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import fbeta_score, make_scorer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+
 #---------------------------------------------------
 samples = pd.read_csv('./DUMMY_CODED.csv')
 
@@ -22,10 +25,29 @@ tuned_parameters = [{'C': cs}]
 n_folds = 3
 scorerFunc=make_scorer(corr_class)
 
-gridL = GridSearchCV(logReg2, tuned_parameters, cv=n_folds, refit=False, scoring = scorerFunc)
+gridL = GridSearchCV(logReg2, tuned_parameters, cv=n_folds, refit=True, scoring = scorerFunc)
 gridL.fit(X_train, y_train)
 
-scoresGrid=gridL.cv_results_['mean_test_score']
-print("Average Per Class Accuracy for each C")
-print(scoresGrid)
-print("\nBest C value: "+ str(cs[np.argmax(scoresGrid)]) + ", Average Per Class Accuracy: "+ str(scoresGrid[np.argmax(scoresGrid)]))
+fpr, tpr, thresholds = roc_curve(gridL.predict(X_test), y_test)
+auc = roc_auc_score(gridL.predict(X_test), y_test)
+
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+# This is the ROC curve
+plt.show() 
+
+print("Test True Positive Rate: ", corr_class(y_test, gridL.predict(X_test)))
+
+#scoresGrid=gridL.cv_results_['mean_test_score']
+#print("Average Per Class Accuracy for each C")
+#print(scoresGrid)
+#print("\nBest C value: "+ str(cs[np.argmax(scoresGrid)]) + ", Average Per Class Accuracy: "+ str(scoresGrid[np.argmax(scoresGrid)]))
